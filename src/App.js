c81b1266039c6f0f3d3fd3a678ce9803b063da2b
Aaron Ault <ault@purdue.edu>
2017-06-15 09:48:03 +0200

    The main site is working

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { TextField, RaisedButton } from 'material-ui'
+import oadaIdClient from 'oada-id-client'
+var domain = '207.154.219.238'
+var token = ''
+
+class App extends Component {
+   
+  doAuth() {
+    var options = {
+      metadata: 'eyJqa3UiOiJodHRwczovL2lkZW50aXR5Lm9hZGEtZGV2LmNvbS9jZXJ0cyIsImtpZCI6ImtqY1NjamMzMmR3SlhYTEpEczNyMTI0c2ExIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ.eyJyZWRpcmVjdF91cmlzIjpbImh0dHBzOi8vdHJpYWxzdHJhY2tlci5vYWRhLWRldi5jb20vb2F1dGgyL3JlZGlyZWN0Lmh0bWwiLCJodHRwOi8vbG9jYWxob3N0OjgwMDAvb2F1dGgyL3JlZGlyZWN0Lmh0bWwiXSwidG9rZW5fZW5kcG9pbnRfYXV0aF9tZXRob2QiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6Y2xpZW50LWFzc2VydGlvbi10eXBlOmp3dC1iZWFyZXIiLCJncmFudF90eXBlcyI6WyJpbXBsaWNpdCJdLCJyZXNwb25zZV90eXBlcyI6WyJ0b2tlbiIsImlkX3Rva2VuIiwiaWRfdG9rZW4gdG9rZW4iXSwiY2xpZW50X25hbWUiOiJUcmlhbHMgVHJhY2tlciIsImNsaWVudF91cmkiOiJodHRwczovL2dpdGh1Yi5jb20vT3BlbkFUSy9UcmlhbHNUcmFja2VyIiwiY29udGFjdHMiOlsiU2FtIE5vZWwgPHNhbm9lbEBwdXJkdWUuZWR1PiJdLCJzb2Z0d2FyZV9pZCI6IjVjYzY1YjIwLTUzYzAtNDJmMS05NjRlLWEyNTgxODA5MzM0NCIsInJlZ2lzdHJhdGlvbl9wcm92aWRlciI6Imh0dHBzOi8vaWRlbnRpdHkub2FkYS1kZXYuY29tIiwiaWF0IjoxNDc1NjA5NTkwfQ.Qsve_NiyQHGf_PclMArHEnBuVyCWvH9X7awLkO1rT-4Sfdoq0zV_ZhYlvI4QAyYSWF_dqMyiYYokeZoQ0sJGK7ZneFwRFXrVFCoRjwXLgHKaJ0QfV9Viaz3cVo3I4xyzbY4SjKizuI3cwfqFylwqfVrffHjuKR4zEmW6bNT5irI',
+      scope: 'feed-intakes slaughter-stats daily-weights',
+      "redirect": 'http://localhost:8000/oauth2/redirect.html',
+    }
+    oadaIdClient.getAccessToken(domain, options, function(err, accessToken) {
+      if (err) { console.dir(err); return err; } // Something went wrong  
+      console.log('DONE')
+      window.location = 'http://localhost:8000/graph.html';
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Swine Smarts</h2>
+        </div>
+        <div className="App-middle">
+          <TextField
+            defaultValue="pigdrive.com"
+            floatingLabelText="Connect Data Source"
+            style={{width:'200px'}}
+          />
+          <RaisedButton 
+            onTouchTap={() => this.doAuth()}
+            style={{width:'200px', 'marginTop': '30px'}}
+            label="Connect to my Data!" />
+          <p className="App-intro">
+          {token} 
+          </p>
+        </div>
+        <div className="App-footer">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
